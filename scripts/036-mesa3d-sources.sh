f05c931729377a434cfd4a55bc946ca1616adfa7
Paweł Kołodziejski <aquadran@gmail.com>
2026-07-11 17:40:00 +0200

    update mesa and drop moltenvk

--- scripts/036-mesa3d-sources.sh
+++ scripts/036-mesa3d-sources.sh
@@ -2,7 +2,7 @@
 
 PKGNAME=mesa
 
-REV=0151fedf47288088b99057db82ff9df65f4d8d93
+REV=ec2a3402c2977ee050aa80eb9cdf1a5b2b678122
 
 if [ ! -d mesa-workspace/${PKGNAME} ]; then
     git clone --branch main https://gitlab.freedesktop.org/mesa/mesa.git mesa-workspace/${PKGNAME} && cd mesa-workspace/${PKGNAME} && git checkout --force $REV && cd ../..
