99e82da3bcbcc4502845a76a118159dad49fa109
Paweł Kołodziejski <aquadran@gmail.com>
2026-08-09 19:09:09 +0200

    faster fetch mesa3d sources

--- scripts/050-mesa3d-sources.sh
+++ scripts/050-mesa3d-sources.sh
@@ -5,6 +5,13 @@ PKGNAME=mesa
 REV=c4039495967db0bff1ffaf2c955bc8586ce9c269
 
 if [ ! -d mesa-workspace/${PKGNAME}-sources ]; then
-    git clone --branch main https://gitlab.freedesktop.org/mesa/mesa.git mesa-workspace/${PKGNAME}-sources && cd mesa-workspace/${PKGNAME}-sources && git checkout --force $REV && cd ../..
+    mkdir -p mesa-workspace/${PKGNAME}-sources
+    pushd mesa-workspace/${PKGNAME}-sources
+    git init
+    git remote add origin https://gitlab.freedesktop.org/mesa/mesa.git
+    git fetch --depth 1 origin ${REV}
+    git checkout FETCH_HEAD
+    popd
+
     if [ -f ../patches/${PKGNAME}.patch ]; then cat ../patches/${PKGNAME}.patch | patch -p1 -d mesa-workspace/${PKGNAME}-sources; fi
 fi
