81553804940e2bcbdf560196647a662e2b976af0
Paweł Kołodziejski <aquadran@gmail.com>
2026-09-20 07:58:36 +0200

    update gnutls

--- scripts/009-gnutls.sh
+++ scripts/009-gnutls.sh
@@ -4,7 +4,7 @@ WINE_LIBS=${WINE_LIBS:=$(PWD)/../target}
 
 PATH=${WINE_LIBS}/bin:${PATH}
 
-VER=3.8.11
+VER=3.8.13
 PKGNAME=gnutls
 
 if [ ! -f ${PKGNAME}-${VER}.tar.xz ]; then wget --continue https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/${PKGNAME}-${VER}.tar.xz; fi
